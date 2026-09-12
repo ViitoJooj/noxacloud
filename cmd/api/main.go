ec5fdad7dd673d76cade5e8485fc9429ad2a3b36
ViitoJooj <joaovitor819oqueres@gmail.com>
2026-09-12 01:43:58 -0300

    feat: wire users container and start Gin HTTP server in main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 
+	"github.com/ViitoJooj/noxacloud/internal/containers/users"
 	"github.com/ViitoJooj/noxacloud/pkg/dotenv"
 	"github.com/ViitoJooj/noxacloud/pkg/errorx"
+	"github.com/ViitoJooj/noxacloud/pkg/migration"
 	"github.com/ViitoJooj/noxacloud/pkg/postgres"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -22,5 +25,16 @@ func main() {
 		errorx.Fatal(err)
 	}
 
-	conn.Ping()
+	err = migration.Migrate(ctx, conn)
+	if err != nil {
+		errorx.Fatal(err)
+	}
+
+	router := gin.Default()
+	users.Container(conn, router)
+
+	addr := cfg.Application.BackHost + ":" + cfg.Application.BackPort
+	if err := router.Run(addr); err != nil {
+		errorx.Fatal(err)
+	}
 }
