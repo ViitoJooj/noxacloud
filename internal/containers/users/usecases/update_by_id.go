19c1b7fef9f06dc2777371aa140ff10ccd57b54c
ViitoJooj <joaovitor819oqueres@gmail.com>
2026-09-14 13:46:37 -0300

    add UpdateUserByIDUseCase

---
+++ internal/containers/users/usecases/update_by_id.go
@@ -0,0 +1,38 @@
+package users_usecases
+
+import (
+	"context"
+	"uuid"
+
+	"github.com/ViitoJooj/noxacloud/internal/containers/users/entities"
+	"github.com/ViitoJooj/noxacloud/internal/contracts"
+)
+
+type UpdateUserByIDUseCase struct {
+	Repository contracts.IRepository[entities.User, entities.User]
+}
+
+func NewUpdateUserByIDUseCase(repository contracts.IRepository[entities.User, entities.User]) UpdateUserByIDUseCase {
+	return UpdateUserByIDUseCase{
+		Repository: repository,
+	}
+}
+
+type UpdateUserByIDUseCaseInput struct {
+	ID   *uuid.UUID
+	Data entities.User
+}
+
+type UpdateUserByIDUseCaseOutput struct {
+	Data entities.User
+}
+
+func (u UpdateUserByIDUseCase) Perform(ctx context.Context, input UpdateUserByIDUseCaseInput) (UpdateUserByIDUseCaseOutput, error) {
+
+	output, err := u.Repository.Update(ctx, input.ID, input.Data)
+	if err != nil {
+		return UpdateUserByIDUseCaseOutput{}, err
+	}
+
+	return UpdateUserByIDUseCaseOutput{Data: output}, nil
+}
