aac2fcbb752616090fdbc24b60c9a0edde802a36
ViitoJooj <joaovitor819oqueres@gmail.com>
2026-09-14 13:46:37 -0300

    regenerate sveltekit internal server file

--- www/.svelte-kit/generated/server/internal.js
+++ www/.svelte-kit/generated/server/internal.js
@@ -26,7 +26,7 @@ export const options = {
 		app: ({ head, body, assets, nonce, env }) => "<!doctype html>\n<html lang=\"en\">\n\t<head>\n\t\t<meta charset=\"utf-8\" />\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n\t\t<meta name=\"text-scale\" content=\"scale\" />\n\t\t" + head + "\n\t</head>\n\t<body data-sveltekit-preload-data=\"hover\">\n\t\t<div style=\"display: contents\">" + body + "</div>\n\t</body>\n</html>\n",
 		error
 	},
-	version_hash: "l66z1w"
+	version_hash: "62u5o5"
 };
 
 export async function get_hooks() {
