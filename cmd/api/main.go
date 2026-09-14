c8669c8a865b34713e3d51a03c223732c6657974
ViitoJooj <joaovitor819oqueres@gmail.com>
2026-09-14 13:46:37 -0300

    wire users.Init and error-handling middleware into the api entrypoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ViitoJooj/noxacloud/internal/containers/users"
 	"github.com/ViitoJooj/noxacloud/pkg/dotenv"
 	"github.com/ViitoJooj/noxacloud/pkg/errorx"
+	"github.com/ViitoJooj/noxacloud/pkg/httpx"
 	"github.com/ViitoJooj/noxacloud/pkg/migration"
 	"github.com/ViitoJooj/noxacloud/pkg/postgres"
 	"github.com/gin-gonic/gin"
@@ -14,27 +15,29 @@ import (
 func main() {
 
 	ctx := context.Background()
+	server := gin.Default()
+	server.Use(httpx.ErrorHandler())
 
 	cfg, err := dotenv.NewDotenv(ctx)
 	if err != nil {
 		errorx.Fatal(err)
 	}
 
-	conn, err := postgres.NewPostgresConn(ctx, cfg.PostgreSQL)
+	db, err := postgres.NewPostgresConn(ctx, cfg.PostgreSQL)
 	if err != nil {
 		errorx.Fatal(err)
 	}
 
-	err = migration.Migrate(ctx, conn)
+	err = migration.Migrate(ctx, db)
 	if err != nil {
 		errorx.Fatal(err)
 	}
 
-	router := gin.Default()
-	users.Container(conn, router)
+	if err := users.Init(ctx, server, db); err != nil {
+		errorx.Fatal(err)
+	}
 
-	addr := cfg.Application.BackHost + ":" + cfg.Application.BackPort
-	if err := router.Run(addr); err != nil {
+	if err := server.Run(":8080"); err != nil {
 		errorx.Fatal(err)
 	}
 }
